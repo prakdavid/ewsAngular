1854bc08a759fe8bed72b8d8fc0234be695f3fbe
David PRAK <prak_d@etna-alternance.net>
2014-02-06 23:03:57 +0100

    All in one fille(css and js) --> uglify, cssmin, concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,17 +5,64 @@ var mountFolder = function (connect, dir) {
     return connect.static(require('path').resolve(dir));
 };
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
 module.exports = function (grunt) {
     // load all grunt tasks
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'app/resources/js/<%= pkg.name %>.js',
+                dest: 'app/resources/js/<%= pkg.name %>.js'
+            }
+        },
+        cssmin: {
+            options: {
+                keepSpecialComments: 0,
+                report: 'min',
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                files: {
+                    "app/resources/css/<%= pkg.name %>.css": ["app/resources/css/*.css"]
+                }
+            }
+        },
+        concat: {
+            options: {
+                stripBanners: true,
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            css: {
+                src: [
+                    "app/components/bootstrap/dist/css/bootstrap.min.css",
+                    "app/components/bootstrap/dist/css/bootstrap-responsive.min.css",
+                    "app/components/font-awesome/css/font-awesome.min.css",
+                    "app/resources/css/all.css"
+                ],
+                dest: "app/resources/css/<%= pkg.name %>.css"
+            },
+            components: {
+                src: [
+                    "app/components/jquery/jquery.min.js",
+                    "app/components/jquery-ui/ui/minified/jquery-ui.min.js",
+                    "app/components/bootstrap/dist/js/bootstrap.min.js",
+                    "app/components/angular/angular.min.js",
+                    "app/components/angular-jquery/dist/angular-jquery.min.js"
+                ],
+                dest: "app/resources/js/<%= pkg.name %>_components.js"
+            },
+            build: {
+                src: [
+                    "app/scripts/**/*.js"
+                ],
+                dest: "app/resources/js/<%= pkg.name %>.js"
+              }
+        },
         watch: {
             options: {
                 nospawn: true
@@ -64,15 +111,22 @@ module.exports = function (grunt) {
                     paths: ['app/components/bootstrap/less', 'app/styles']
                 },
                 files: {
-                    'app/styles/main.css': 'app/styles/main.less'
+                    'app/resources/css/main.css': 'app/resources/css/main.less'
                 }
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
     grunt.registerTask('server', function (target) {
 
         grunt.task.run([
+            'concat:css',
+            'concat:components',
+            'concat:build',
             'less:server',
             'connect:livereload',
             'open',
---
+++ app/resources/js/ews.js
@@ -0,0 +1,6 @@
+/*! ews 2014-02-06 */
+var app = angular.module('myApp', []);
+
+app.run(function($rootScope) {
+    $rootScope.name = "David";
+});
